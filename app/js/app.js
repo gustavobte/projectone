032c0fe68592f8d132e794edc4a7af02b62b2d1d
Rosa Paiva Silva <grosap@indra.es>
2016-11-22 17:43:18 -0200

    Menu BuscaLote

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var app = angular.module('myApp', ['ngRoute','ngMaterial','ui.materialize'
-    ,'myApp.home', 'myApp.buscaPessoas', 'myApp.resultadoBusca', 'myApp.login','myApp.resultadoBuscaPj','myApp.buscaPessoasPj', 'myApp.controllers' , 'myApp.services']).
+    ,'myApp.home', 'myApp.buscaPessoas','myApp.buscaLote', 'myApp.resultadoBusca', 'myApp.login','myApp.resultadoBuscaPj','myApp.buscaPessoasPj', 'myApp.controllers' , 'myApp.services']).
 
 
 config(['$routeProvider', function($routeProvider) {
--- app/views/buscaLote/buscaLote.js
+++ app/views/buscaLote/buscaLote.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var app = angular.module('myApp.buscaPessoas', ['ngRoute','ngMaterial','ui.materialize', 'ui.mask'])
+var app = angular.module('myApp.buscaLote', ['ngRoute','ngMaterial','ui.materialize', 'ui.mask'])
 
 
 // Declared route
     app.config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/buscaPessoas', {
-            templateUrl: 'views/buscaPessoas/buscaPessoas.html',
-            controller:  'EndSofiaCtrl',
+        $routeProvider.when('/buscaLote', {
+            templateUrl: 'views/buscaLote/buscaLote.html',
+            controller:  'buscaLoteCtrl',
             controllerAs: 'vm'
         });
     }])
