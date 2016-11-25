e199d9204d57ec30ffc359dfe6bc6ada6e424a10
felansu <gaferran@gmail.com>
2016-11-25 16:02:18 -0200

    Acrescentada biblioteca angular-ui e filter para não mostrar endereços duplicados na tela de buscaLote.html

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 var app = angular.module('myApp', ['ngCsv', 'ngSanitize', 'ngRoute','ngMaterial','ui.materialize',
   'myApp.home', 'myApp.buscaPessoas','myApp.buscaLote', 'myApp.resultadoBusca',
   'myApp.login','myApp.resultadoBuscaPj','myApp.buscaPessoasPj', 'myApp.controllers',
-  'myApp.services', 'myApp.filters']).
+  'myApp.services', 'myApp.filters','ui.directives','ui.filters']).
 
 
 config(['$routeProvider', function($routeProvider) {
