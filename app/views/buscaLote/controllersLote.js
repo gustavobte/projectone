53ce52ecc49a4cf9138fc6c7292f47d919bdd78d
felansu <gaferran@gmail.com>
2016-11-29 00:25:27 -0200

    Acescentada funcionalidade para carregar arquivo. Melhorado filtro de CPF/CNPJ. Incluidos dados numero de documento e telefone na tela de pes. lote.

--- app/views/buscaLote/controllersLote.js
+++ app/views/buscaLote/controllersLote.js
@@ -11,6 +11,12 @@ controladores.controller('BuscaLoteCtrl', function($rootScope, $location, $filte
     vm.delimiter = ',';
     vm.cardsPraExportacao = [];
     vm.chek = false;
+    vm.carregado = false;
+
+    $scope.carregarConteudo = function($fileContent){
+       vm.cpf = $fileContent;
+       vm.carregado = "Carregado com sucesso";
+   };
 
     $scope.submitTable = function() {
         console.log($scope.table);
@@ -20,6 +26,11 @@ controladores.controller('BuscaLoteCtrl', function($rootScope, $location, $filte
         ResultadoService.setPessoa(ec_id);
     };
 
+    vm.filtraCpfs = function(cpfs) {
+        var cpfFiltrado = new RegExp("[^\\d\\"+vm.delimiter+"]", "g");
+        return cpfs.replace(cpfFiltrado, "")
+    };
+
     $scope.listarEcByListaCPF = function() {
         $scope.loading = true;
 
@@ -27,17 +38,17 @@ controladores.controller('BuscaLoteCtrl', function($rootScope, $location, $filte
             cpf: []
         };
 
-        var cpf = vm.cpf.replace(/[^\d+\,\|\.]/g, '').split(vm.delimiter)
+        var cpfs = vm.cpf.replace(/\n/g, vm.delimiter)
+        cpfs= vm.filtraCpfs(cpfs).split(vm.delimiter);
 
-        for (var i = 0; i < cpf.length; i++) {
-            var zeros = cpf[i].length == 11 ? '000' : ''
-            vm.dados.cpf.push("'" + zeros + cpf[i] + "'");
+        for (var i = 0; i < cpfs.length; i++) {
+            var zeros = cpfs[i].length == 11 ? '000' : ''
+            vm.dados.cpf.push("'" + zeros + cpfs[i] + "'");
         }
 
         LoteSofiaService.listarEcByListaCPF(vm.dados.cpf).then(
 
             function(dados) {
-                debugger
                 if (dados != "") {
                     vm.dados = vm.formataPessoa(JSON.parse(dados)["values"]);
                 } else {
@@ -60,13 +71,25 @@ controladores.controller('BuscaLoteCtrl', function($rootScope, $location, $filte
     vm.formataPessoa = function(dados) {
         var listaPessoas = [];
         for (var i = 0; i < dados.length; i++) {
+
+          var numDocumento;
+
+          if(dados[i][2]=="J"){
+            numDocumento = dados[i][3]
+          }else{
+            numDocumento = dados[i][3].substring(3,14)
+          }
+
             var pessoa = {
                 "nome": dados[i][4],
                 "logradouro": dados[i][5] + " " + dados[i][6],
                 "quadraLote": dados[i][10],
                 "bairro": dados[i][11],
                 "estado": dados[i][13] + "-" + dados[i][14],
-                "cep": dados[i][12]
+                "cep": dados[i][12],
+                "telefone": dados[i][20],
+                "numDocumento": numDocumento,
+                "tipoPessoa": dados[i][2]
             };
             var duplicado = $filter("filter")(listaPessoas, {
                 nome: pessoa.nome
@@ -105,6 +128,12 @@ controladores.controller('BuscaLoteCtrl', function($rootScope, $location, $filte
         }
         if (pessoa.cep == "") {
             delete pessoa.cep
+        }
+        if (pessoa.telefone == "") {
+            delete pessoa.telefone
+        }
+        if (pessoa.numDocumento == "") {
+            delete pessoa.numDocumento
         }
         return pessoa;
     }
