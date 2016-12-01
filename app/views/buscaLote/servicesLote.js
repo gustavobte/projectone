c9bd6ddb34bd0e32f4d673f8d649d74e4f0fcaab
felansu <gaferran@gmail.com>
2016-12-01 16:56:47 -0200

    [FIX] Bug para busqueda de elementos sem agrupar

--- app/views/buscaLote/servicesLote.js
+++ app/views/buscaLote/servicesLote.js
@@ -2,22 +2,6 @@
 servicos.factory('LoteSofiaService', function ($q, SofiaService) {
     var ontologia = "ec_eck_max_mfinal_by_numdocumento";
 
-    var listarEcByDocumento = function (documentos, tipoPessoa) {
-        var q = $q.defer();
-
-        var query = "SELECT * FROM " + ontologia + " WHERE " + ontologia + ".ec_numdocumento IN (" + documentos + ")";
-        query = query.concat(" AND " + ontologia + ".ec_tipopessoa = " + tipoPessoa);
-
-        SofiaService.listar(query, ontologia).then(
-            function (dados) {
-                q.resolve(dados);
-            },
-            function (dados) {
-                q.reject(dados);
-            });
-        return q.promise;
-    };
-
     var listarEcByListaDocumentos = function (documentos, tipoPessoa) {
         return $q.all(documentos.map(function (item) {
             var query = "SELECT * FROM " + ontologia + " WHERE " + ontologia + ".ec_numdocumento IN (" + item + ")";
@@ -39,10 +23,9 @@ servicos.factory('LoteSofiaService', function ($q, SofiaService) {
 
     var listarEc = function (documentos, tipoDocumento) {
         var tipoPessoa = tipoDocumento == "cpf" ? "'F'" : "'J'";
-        return documentos.length == 1 ? listarEcByDocumento(documentos, tipoPessoa) : listarEcByListaDocumentos(documentos, tipoPessoa);
+        return listarEcByListaDocumentos(documentos, tipoPessoa);
     };
     return {
-        listarEcByDocumento: listarEcByDocumento,
         listarEcByListaDocumentos: listarEcByListaDocumentos,
         listarEc: listarEc
     }
